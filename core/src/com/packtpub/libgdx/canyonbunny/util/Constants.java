c118b0bb5166d69132104ad0e322859a468043fc
Kirill Bogdanov <baghdad232@gmail.com>
2016-10-14 00:04:14 +0300

    init

---
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -0,0 +1,14 @@
+package com.packtpub.libgdx.canyonbunny.util;
+
+/**
+ * Author: Kirill Bogdanov
+ * Date: 01.06.2016.
+ */
+public class Constants {
+    // Visible game world is 5 meters wide
+    public static final float VIEWPORT_WIDTH = 5.0f;
+    // Visible game world is 5 meters tall
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+    // Location of description file for texture atlas
+    public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
+}
